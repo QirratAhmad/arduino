f192e3fcdb77e411da6b7e95b71f84d6b8a106bf
Qirrat Ahmad <qirratahmad@seoulchristian.org>
2017-01-10 10:29:47 +0900

    Edit code to make the external LED blink

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,12 +1,28 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(12, INPUT);//The mode is output
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  digitalWrite(13, HIGH);//The light is on
+  delay(1000);//The duration is 1 second
+  digitalWrite(13, LOW);//The light is off
+  delay(2000);//The duration is 2 seconds
+  digitalWrite(13, HIGH);//The light is on
+  delay(3000);//The duration is 3 seconds
+  digitalWrite(13, LOW);//The light is off
+  delay(4000);//The duration is 4 seconds
+  digitalWrite(13, HIGH);//The light is on
+  delay(5000);//The duration s 5 seconds
+  digitalWrite(13, LOW);//The light is off
+  delay(6000);//The duration is 6 seconds
+  digitalWrite(13, HIGH);//The light is on
+  delay(4000);//The duration is 4 seconds
+  digitalWrite(13, LOW);//The light is off
+  delay(5000);//The duration is 5 seconds
+  digitalWrite(13, HIGH);//The light is on
+  delay(7000);//The duration is 7 seconds
+  digitalWrite(13, LOW);//The light is off
+  delay(2000);//The duration is 2 seconds
 }
