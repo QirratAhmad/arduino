5a0f1fa61847481344aa807555497e57159d0657
Qirrat Ahmad <qirratahmad@seoulchristian.org>
2017-01-19 10:26:47 +0900

    Add comments to forFde and ifFade

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -1,9 +1,17 @@
 void setup() {
+  pinMode (9, OUTPUT); // This is the pin number and mode
+  Serial.begin(9600); // Activates serial connection and sets the baud rate to 9,600
+}
 
-  pinMode (9, OUTPUT);
-  Serial.begin(9600);
-
-void loop() 
-
-\
-
+void loop() {
+  for (int lightValue = 0; lightValue <= 255; lightValue++) { // This is the initialization, test, and increment/decrement
+    analogWrite(9, lightValue); // This is the pin and value
+    Serial.println(lightValue); // Set output lightValue to LED
+    delay(10); // The duration is 10 milliseconds
+  }
+  for (int lightValue = 255; lightValue >= 0; lightValue--) { // This is the initialization, test, and increment/decrement
+    analogWrite(9, lightValue); // Sends integer to pin 9 in analog form
+    Serial.println(lightValue); // Sends integer to serial monitor
+    delay(10); // Waits for 10 milliseconds
+  }
+}
--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -1,17 +1,17 @@
-int lightValue = 0;
-int delta = 1;
+int lightValue = 0; // Set value of lightValue to 0
+int delta = 1; // Set value of delta to 1
 
 void setup() {
-  Serial.begin(9600);
-  pinMode (9, OUTPUT);
+  Serial.begin(9600); // Activates serial connection and sets the baud rate to 9,600
+  pinMode (9, OUTPUT); // This is the mode and pin number
 }
 
 void loop() {
-  lightValue = lightValue + delta;
-  if (lightValue <= 0 || lightValue >= 255) {
-    delta = -delta;
+  lightValue = lightValue + delta; // Tell the values and premises for the statement to be true
+  if (lightValue <= 0 || lightValue >= 255) { // If these are true, only then will the light fade in and out
+    delta = -delta; // The variable negates the value of delta
   }
-  analogWrite(9, lightValue);
-  Serial.println(lightValue);
-  delay(10);
+  analogWrite(9, lightValue); // Sends integer to pin 9 in analog form
+  Serial.println(lightValue); // Sends integer to serial monitor
+  delay(10); // Waits for 10 milliseconds
 }
